b49428f1d5409d2000884e2637d917d4b58edb60
Vamsi Nallapareddy <vamsinallapareddy@gmail.com>
2020-10-17 11:11:30 +0530

    folder structure

--- src/ann.py
+++ src/ann.py
@@ -1,8 +1,23 @@
+import os
+####*IMPORANT*: Have to do this line *before* importing tensorflow
+os.environ['PYTHONHASHSEED']=str(1)
+
 # libraries
 import numpy as np 
+import tensorflow as tf 
+import random
 import keras
 from keras.models import Model
 from keras.layers import Dense, Flatten, Dropout
+
+def reset_random_seeds():
+   os.environ['PYTHONHASHSEED']=str(1)
+   tf.random.set_seed(1)
+   np.random.seed(1)
+   random.seed(1)
+
+#make some random data
+reset_random_seeds()
 
 # data parameters
 num_classes = 10
@@ -17,8 +32,6 @@ x_test = x_test.astype("float32")/255
 
 x_train = np.reshape(x_train, (60000, -1))
 x_test = np.reshape(x_test, (10000, -1))
-
-print(y_test[0])
 
 def ANN():
 	inp = keras.Input(shape = x_train[0].shape)
@@ -35,7 +48,7 @@ classifier = ANN()
 batch_size = 32
 epochs = 100
 
-mcp_save = keras.callbacks.callbacks.ModelCheckpoint('models/dl1.h5', save_best_only=True, monitor='val_accuracy', verbose=1)
+mcp_save = keras.callbacks.callbacks.ModelCheckpoint('../models/dl1.h5', save_best_only=True, monitor='val_accuracy', verbose=1)
 reduce_lr = keras.callbacks.callbacks.ReduceLROnPlateau(monitor='val_accuracy', factor=0.1, patience=10, verbose=1, mode='auto', min_delta=0.0001, cooldown=0, min_lr=0)
 callbacks_list = [mcp_save, reduce_lr]
 
